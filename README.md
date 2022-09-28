d74847cedc2f6ee807c3f7236c4a51f671c0fbda
Enrico Zimuel <e.zimuel@gmail.com>
2022-09-28 22:29:06 +0200

    Primo esercizio sui merkle tree

---
+++ README.md
@@ -0,0 +1,9 @@
+## Laboratorio di Tecnologie Blockchain
+
+Questo repository contiene le esercitazioni svolte durante il corso di **Tecnologie Blockchain** tenuto dal docente [Enrico Zimuel](https://www.zimuel.it/) presso l'[ITS-ICT Piemonte](https://www.its-ictpiemonte.it/) di Torino.
+
+### Markle Tree
+
+Scrivere un programma in Python che sia in grado di leggere un insieme di file (in numero pari) e calcolare il [Merkle tree](https://en.wikipedia.org/wiki/Merkle_tree) root utilizzando la funzione hash SHA256.
+
+Soluzione in [merkle/merkle_tree.py](merkle/merkle_tree.py)
---
+++ merkle/merkle_tree.py
@@ -0,0 +1,43 @@
+# Esercizio sui Merkle tree
+#
+# Laboratorio di Tecnologie Blockchain - Docente: Enrico Zimuel
+# ITS ICT Piemonte (https://www.its-ictpiemonte.it/)
+
+from hashlib import sha256
+from os import listdir
+import os.path
+
+# Prendo tutti i file nella cartella tranne questo script in python
+script_dir = os.path.dirname(__file__)
+files = [f for f in listdir(script_dir) if f != os.path.basename(__file__)]
+
+# Ordino i file per nome
+files.sort()
+print("File in input del Merkle Tree:")
+print(files)
+
+if (len(files) % 2 != 0):
+    print("Errore: il numero di file deve essere pari")
+    exit(1)
+
+# Calcolo i valori SHA256 di ogni file
+hashes = []
+for f in files:
+    content = ''
+    with open(f, 'r') as file:
+        content += file.read()
+    hashes.append(sha256(content.encode('utf-8')).hexdigest())
+
+print("SHA256 dei file:")
+print(hashes)
+
+# Calcolo il merkle tree root
+merkle = hashes
+while len(merkle) > 1:
+    new_merkle = []
+    for i in range(0,len(merkle),2):
+        content = merkle[i] + merkle[i+1] 
+        new_merkle.append(sha256(content.encode('utf-8')).hexdigest())
+    merkle = new_merkle
+
+print("Merkle tree root: " + merkle[0])
